3b252f5241bb1679b428ffe7b1f09726b86fef4e
waldemarciupa <waldemarciupa90@gmail.com>
2023-11-01 22:41:54 +0100

    display variants

--- src/ui/molecules/ProductItem.tsx
+++ src/ui/molecules/ProductItem.tsx
@@ -42,6 +42,24 @@ export const ProductItem = async ({ id }: ProductItemProps) => {
 				)}
 				<div className="mt-4 sm:mx-6 sm:mt-0">
 					<ProductItemDescription product={product} />
+					{product.variants.length > 0 && (
+						<div className="mt-4">
+							<label htmlFor="variants" className="block text-sm font-medium text-gray-700">
+								Variants
+							</label>
+							<select
+								id="variants"
+								name="variants"
+								className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+							>
+								{product.variants.map((variant) => (
+									<option key={variant.id} value={variant.id}>
+										{variant.name}
+									</option>
+								))}
+							</select>
+						</div>
+					)}
 					<form action={addToCartAction}>
 						<input type="hidden" name="productId" value={product.id} />
 						<AddToCartButton />
