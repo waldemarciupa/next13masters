4ca9ff3c901843e888ad9d031d48867256b32e34
waldemarciupa <waldemarciupa90@gmail.com>
2023-12-22 06:57:57 +0100

    add zod schema validation

--- src/app/review/actions.ts
+++ src/app/review/actions.ts
@@ -1,23 +1,39 @@
 "use server";
-
+import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { type ReviewCreateInput } from "@/gql/graphql";
 import { createReview } from "@/api/review";
 
+const schema = z.object({
+	content: z.string().min(3, { message: "Content must be 3 or more character long" }).max(300),
+	email: z.string().email(),
+	headline: z.string().min(3, { message: "Headline must be 3 or more character long" }).max(100),
+	name: z.string().min(3, { message: "Name must be 3 or more character long" }).max(100),
+	product: z.object({ connect: z.object({ id: z.string() }) }),
+	rating: z.number().int().min(1).max(5),
+});
+
 export async function addReview(currentState: unknown, formData: FormData) {
-	const data: ReviewCreateInput = {
+	const validatedFields = schema.safeParse({
 		content: formData.get("content") as string,
 		email: formData.get("email") as string,
 		headline: formData.get("headline") as string,
 		name: formData.get("name") as string,
 		product: { connect: { id: formData.get("productId") as string } },
 		rating: 5,
-	};
+	});
 
-	try {
-		await createReview(data);
-		revalidatePath("/product/[id]", "page");
-	} catch (error) {
-		return "Failed to add review";
+	if (!validatedFields.success) {
+		return validatedFields.error.flatten();
+	} else {
+		const data = validatedFields.data as ReviewCreateInput;
+
+		try {
+			await createReview(data);
+			revalidatePath("/product/[id]", "page");
+			return { message: "Review added" };
+		} catch (error) {
+			return { message: "Failed to add review" };
+		}
 	}
 }
