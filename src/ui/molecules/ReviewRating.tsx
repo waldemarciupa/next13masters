66a4f451ae324a82b74c4b11bdcaa44cdb123700
waldemarciupa <waldemarciupa90@gmail.com>
2023-12-29 14:59:38 +0100

    use review star component

--- src/ui/molecules/ReviewRating.tsx
+++ src/ui/molecules/ReviewRating.tsx
@@ -1,34 +1,17 @@
-import { Star } from "lucide-react";
+import { useState } from "react";
+import { ReviewStar } from "./ReviewStar";
 import { ReviewHeadline } from "@/ui/atoms/ReviewHeadline";
 
 export const ReviewRating = () => {
-	{
-		/* TODO: Implement rating */
-	}
+	const [rating, setRating] = useState(0);
+
 	return (
 		<div className="mb-4">
 			<ReviewHeadline htmlFor="rating">Rating</ReviewHeadline>
 			<fieldset className="flex gap-x-2">
-				<label htmlFor="rating-1">
-					<Star color="grey" strokeWidth={1} />
-				</label>
-				<input type="radio" className="sr-only" id="rating-1" name="rating" value={1} />
-				<label htmlFor="rating-2">
-					<Star color="grey" strokeWidth={1} />
-				</label>
-				<input type="radio" className="sr-only" id="rating-2" name="rating" value={2} />
-				<label htmlFor="rating-3">
-					<Star color="grey" strokeWidth={1} />
-				</label>
-				<input type="radio" className="sr-only" id="rating-3" name="rating" value={3} />
-				<label htmlFor="rating-4">
-					<Star color="grey" strokeWidth={1} />
-				</label>
-				<input type="radio" className="sr-only" id="rating-4" name="rating" value={4} />
-				<label htmlFor="rating-5">
-					<Star color="grey" strokeWidth={1} />
-				</label>
-				<input type="radio" className="sr-only" id="rating-5" name="rating" value={5} />
+				{[1, 2, 3, 4, 5].map((r) => (
+					<ReviewStar key={r} r={r} rating={rating} setRating={setRating} />
+				))}
 			</fieldset>
 		</div>
 	);
