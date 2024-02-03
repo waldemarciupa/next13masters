bc4557e04e8e4f5ba6c010d2792ef358402f2286
waldemarciupa <waldemarciupa90@gmail.com>
2024-02-03 22:19:24 +0100

    cart delete order item

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -1,12 +1,20 @@
 "use server";
 
 import { executeGraphql } from "@/api/executeGraphql";
-import { CartRemoveProductDocument, CartSetProductQuantityDocument } from "@/gql/graphql";
+import { CartDeleteOrderItemDocument, CartSetProductQuantityDocument } from "@/gql/graphql";
 
-export const removeItem = (itemId: string) => {
-	console.log("removeItem", itemId);
+export const removeItem = (
+	orderId: string,
+	orderItemId: string,
+	total: number,
+	itemTotal: number,
+) => {
+	const updatedTotal = total - itemTotal;
 
-	return executeGraphql({ query: CartRemoveProductDocument, variables: { itemId } });
+	return executeGraphql({
+		query: CartDeleteOrderItemDocument,
+		variables: { orderId, orderItemId, total: updatedTotal },
+	});
 };
 
 export const changeItemQuantity = (itemId: string, quantity: number) => {
