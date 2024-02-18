374d693f4e745ae2cd58bef6ec322ad6d2a87a73
waldemarciupa <waldemarciupa90@gmail.com>
2024-02-18 20:59:43 +0100

    update public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,19 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+	publicRoutes: [
+		"/",
+		"/search",
+		"/cart",
+		"/products",
+		"/products/(.*)",
+		"/product/(.*)",
+		"/categories/",
+		"/categories/(.*)",
+		"/collections/",
+		"/collections/(.*)",
+	],
+});
 
 export const config = {
 	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
