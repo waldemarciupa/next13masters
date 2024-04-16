82a488d8d2e4d865812f142890b686e7af600d07
waldemarciupa <waldemarciupa90@gmail.com>
2024-04-16 20:25:45 +0200

    Update cart page with payment functionality

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,5 @@
 import { redirect } from "next/navigation";
+import { handlePaymentAction } from "./actions";
 import { getCartByCookieId } from "@/api/cart";
 import { CartList } from "@/ui/molecules/CartList";
 import { type CartFragment } from "@/gql/graphql";
@@ -16,14 +17,14 @@ export default async function Page() {
 			<h1 className="text-3xl font-bold tracking-tight text-slate-900">Your Shopping Cart</h1>
 			<CartList cart={cart} />
 			<CartTotal total={cart.total} />
-			<div className="flex w-full justify-end">
+			<form className="flex w-full justify-end" action={handlePaymentAction}>
 				<button
 					disabled={!cart.total}
-					className="mt-8 w-full rounded-md bg-slate-900 py-3 text-sm font-medium text-white transition-colors duration-300 ease-in-out hover:bg-slate-800 hover:text-white disabled:cursor-not-allowed disabled:bg-slate-400 sm:w-3/6"
+					className="mt-8 w-full max-w-xs rounded-md bg-slate-900 py-3 text-sm font-medium text-white transition-colors duration-300 ease-in-out hover:bg-slate-800 hover:text-white disabled:cursor-not-allowed disabled:bg-slate-400 sm:w-3/6"
 				>
-					Checkout
+					Pay
 				</button>
-			</div>
+			</form>
 		</div>
 	);
 }
