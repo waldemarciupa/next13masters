e00cc9e6d63d260fe2b54129877c2ac7a5e5e92b
waldemarciupa <waldemarciupa90@gmail.com>
2024-04-17 19:58:49 +0200

    Add ErrorPage component for displaying error messages

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ErrorPage() {
+	return (
+		<div className="pt-20 text-center">
+			<h1>Something went wrong</h1>
+			<p>Please try again later</p>
+		</div>
+	);
+}
