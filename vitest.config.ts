abb7aff561dfcf39f073495d30e006bfdbdcb701
waldemarciupa <waldemarciupa90@gmail.com>
2024-04-20 21:54:52 +0200

    Update vitest.config.ts with resolve alias and test configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": "/src",
+		},
+	},
 	test: {
 		environment: "jsdom",
+		includeSource: ["src/**/*.ts", "src/**/*.tsx"],
+		exclude: ["tests", "node_modules", "dist", "cypress", "**/*.config.*"],
 	},
 });
